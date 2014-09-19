9d64f1d36a1b5e2117dcaa2715036e6b5f4800ab
rink72 <beardmans72@gmail.com>
2014-09-19 17:33:51 +1200

    TrainNNet BatchPredict Update
    Changed so that we keep a record of all models ever and the batch
    prediction will not regenerate data. Also deleted the extra
    BatchTrainPlacing trainer. Will work on that later if we want to.

--- R/Testing/TrainNNet.r
+++ R/Testing/TrainNNet.r
@@ -233,7 +233,7 @@ BatchPredict <- function()
 {
 
 	racingConn = odbcConnect("racingTest")
-	networkQuery = "SELECT id, nnfilename, query, Normalised, Scaled from NETWORKDETAILS WHERE ID NOT IN (SELECT DISTINCT NNID FROM PREDICTEDRESULTS)"
+	networkQuery = "SELECT id, nnfilename, query, Normalised, Scaled from NETWORKDETAILS WHERE ID NOT IN (SELECT DISTINCT NNID FROM PREDICTEDRESULTS) and ID not in (SELECT DISTINCT NNID FROM PREDICTEDRESULTS)"
 	networks = sqlQuery(racingConn, networkQuery)
 	print(networks)
 	
@@ -270,102 +270,4 @@ BatchPredict <- function()
 	
 	close(racingConn)
 
-}
-
-BatchTrainPlacing <-function()
-{
-	fileRoot = "C:/Projects/Racing/Networks/Testing/"
-	
-	queries = dim(1)
-	queries[1] = "[D_1_M],[D_2_M],[D_3_M],[D_1_6M],[D_2_6M],[D_3_6M],[D_1D_A],[D_2D_A],[D_3D_A],[D_P_A],[D_P_M],[D_PT_A],[D_PB_A],[D_PWC_A],[D_PTC_A],[D_1MBREAK],[D_6MBREAK],[AD_AVGPP_M],[AD_AVGPP_A],[AD_AVGFP_M],[AD_AVGFP_A]"
-	
-	errors = dim(1)
-	errors[1] = 0.005
-	errors[2] = 0.001
-	
-	racingConn = odbcConnect("racingTest")
-	iterationQuery = "SELECT CONFIGVALUE FROM CONFIG WHERE CONFIGITEM = 'TRAININGITERATION'"
-	iteration = sqlQuery(racingConn, iterationQuery)
-	iteration = iteration[,1] + 1
-	
-	for (iter in iteration:(iteration + 10))
-	{
-		updateIteration = paste("UPDATE CONFIG SET CONFIGVALUE =", iter, "WHERE CONFIGITEM = 'TRAININGITERATION'", sep = " ")
-		update = sqlQuery(racingConn, updateIteration)
-		for(query in 1:length(queries))
-		{
-			for(error in 1:length(errors))
-			{
-				print(paste("Query", query, iter, errors[error], sep="_"))
-				finalQuery = paste("SELECT TOP 5000 RACEID, DOGID,", queries[query], ", RESULT FROM TRAININGDATA WHERE DATATYPE = 'TRAINING'", sep = " ")
-				queryData = sqlQuery(racingConn, finalQuery)
-				queryData = queryData[,3:ncol(queryData)]
-				queryData = queryData[sample(nrow(queryData)),]
-				queryData$RESULT[queryData$RESULT < 4] = 1
-				queryData$RESULT[queryData$RESULT > 3] = -1
-				queryData = normalize(queryData)
-				queryData = data.matrix(queryData)
-				
-				vars = colnames(queryData[,1:ncol(queryData)-1])
-				formula = as.formula(paste("RESULT ~ ", paste(vars, collapse= "+")))
-				net = neuralnet(formula, queryData, hidden = c(2), threshold = errors[error], stepmax = 5000000, lifesign = "full", lifesign.step = 10000, act.fct = "tanh")
-				
-				if(!is.null(net$net))
-				{
-					netDetails = matrix(nrow=1,ncol=8)
-					colnames(netDetails) = c("query", "error", "traininglines", "neurons", "nnfilename", "id", "PredictionType", "ActivationFunction")
-					netDetails[1,1] = queries[query]
-					netDetails[1,2] = errors[error]
-					netDetails[1,3] = 5000
-					netDetails[1,4] = "2"
-					fileName = paste(fileRoot, "Q", query, "E", errors[error], 5000, iter, "N_2.r", sep="_")
-					netDetails[1,5] = fileName
-					NNID = paste("Q", query, "E", errors[error], 5000, iter, "N_2", sep="_")
-					netDetails[1,6] = NNID
-					netDetails[1,7] = "PLACING"
-					netDetails[1,8] = "tanh"
-					
-					netDetails = as.data.frame(netDetails)
-
-					save(net, file = fileName)
-					sqlSave(racingConn, netDetails, tablename = "NETWORKDETAILS", append=TRUE)
-					
-				
-				}
-				
-				net = neuralnet(formula, queryData, hidden = c(1), threshold = errors[error], stepmax = 5000000, lifesign = "full", lifesign.step = 10000, act.fct ="tanh")
-				
-				if(!is.null(net$net))
-				{
-					netDetails = matrix(nrow=1,ncol=8)
-					colnames(netDetails) = c("query", "error", "traininglines", "neurons", "nnfilename", "id", "PredictionType", "ActivationFunction")
-					netDetails[1,1] = queries[query]
-					netDetails[1,2] = errors[error]
-					netDetails[1,3] = 5000
-					netDetails[1,4] = "1"
-					fileName = paste(fileRoot, "Q", query, "E", errors[error], 5000, iter, "N_1.r", sep="_")
-					netDetails[1,5] = fileName
-					NNID = paste("Q", query, "E", errors[error], 5000, iter, "N_1", sep="_")
-					netDetails[1,6] = NNID
-					netDetails[1,7] = "PLACING"
-					netDetails[1,8] = "tanh"
-					
-					netDetails = as.data.frame(netDetails)
-
-					save(net, file = fileName)
-					sqlSave(racingConn, netDetails, tablename = "NETWORKDETAILS", append=TRUE)
-					
-					
-					
-				}
-				
-
-				
-			}
-		}
-	
-	}
-	
-	close(racingConn)
-	return(netDetails)
 }
